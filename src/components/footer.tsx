97ea94122fc0e281d34456431e3c43bb80011df0
lele-maxwell <leleivanlele22@gmail.com>
2025-07-29 12:25:12 +0100

    refactor: update translation hook to use useTranslateWithFallback for improved handling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import Link from 'next/link'
-import { useTranslate } from '@tolgee/react'
+import { useTranslateWithFallback } from '../lib/translations'
 
 export function Footer() {
-  const { t } = useTranslate()
+  const { t } = useTranslateWithFallback()
 
   return (
     <footer className="bg-gradient-to-r from-orange-500 to-orange-600 text-white">
