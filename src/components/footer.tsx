a1169780c83fa3b70fe93db97a3450760cb773b8
lele-maxwell <leleivanlele22@gmail.com>
2025-07-29 08:12:38 +0100

    feat: update contact information in Footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -136,19 +136,19 @@ export function Footer() {
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/>
                 </svg>
-                <span className="text-orange-100 text-sm">hello@chefmaster.com</span>
+                <span className="text-orange-100 text-sm">chefmaster@gmail.com</span>
               </div>
               <div className="flex items-center space-x-2">
                 <svg className="w-4 h-4 text-orange-200" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"/>
                 </svg>
-                <span className="text-orange-100 text-sm">San Francisco, CA</span>
+                <span className="text-orange-100 text-sm">Cameroon, Bangante</span>
               </div>
               <div className="flex items-center space-x-2">
                 <svg className="w-4 h-4 text-orange-200" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
                 </svg>
-                <span className="text-orange-100 text-sm">+1 (555) 123-4567</span>
+                <span className="text-orange-100 text-sm">+237 6707896587</span>
               </div>
               
               {/* Newsletter Signup */}
